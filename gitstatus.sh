ab0264579baa382c3c28a6c8022d4c13ab9201d7
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    modified script to run as a loop

--- gitstatus.sh
+++ gitstatus.sh
@@ -1,43 +1,9 @@
-#! bin/bash
-# Check if there are pending commits from the remote repository
-cd $HOME/repos/pwshpro
-git fetch
-git status
-
-if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
-    # Send a notification
-    notify-send "There are pending commits from the remote repository: pwshpro"
-fi
-
-cd $HOME/repos/startpage
-git fetch
-git status
-
-if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
-    notify-send "There are pending commits from the remote repository: startpage"
-fi
-
-cd $HOME/repos/startpagev2
-git fetch
-git status
-
-
-if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
-    notify-send "There are pending commits from the remote repository: startpagev2"
-fi
-
-cd $HOME/repos/tagalogpy
-git fetch
-git status
-
-if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
-    notify-send "There are pending commits from the remote repository: tagalogpy"
-fi
-
-cd $HOME/repos/bashscripts
-git fetch
-git status
-
-if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
-    notify-send "There are pending commits from the remote repository: bashscripts"
-fi
+#!bin/bash
+# Loops through all folders in the repos directory, running git fetch and git status and sending a notification if there are pending changes.
+for d in $HOME/repos/*/; do
+	cd "$d"
+	git fetch
+	if [[ $(git status | grep "Your branch is behind") ]]; then
+		notify-send "There are pending changes to a remote repository: $(basename $d)"
+	fi
+done
