03bb6d9dae8bd9ceac9d3cc0f8657d3c08cd1d47
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    reformatted, made less verbose and sexier

--- gitmerge.sh
+++ gitmerge.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-# Loops through all folders in the $HOME/repos directory, running git fetch and saving the result of git status to a variable, which is then read and a notification is sent based on the git repo's status.
+# Loops through all folders in the $HOME/repos directory, running 
+# git fetch and saving the result of git status to a variable, which 
+# is then read and a notification is sent based on the git repo's status.
+
 for dir in $HOME/repos/*/; do
-	cd "$dir"
-	echo "Entering $dir"
-	git fetch
-	echo "Changes have been fetched"
-	git_results="$(git status)"
-	if [[ $(echo "$git_results" | grep "Your branch is behind") ]]; then
-		echo "Sending notification: branch is behind"
-		notify-send "Branch is behind: $(basename $dir)" "Changes are being merged now"
-		git merge
-	fi
+  cd "$dir"
+  echo "Entering $dir"
+  git fetch
+  echo "Changes have been fetched"
+  git_results="$(git status)"
+  if [[ $(echo "$git_results" | grep "Your branch is behind") ]]; then
+    echo "Sending notification: branch is behind"
+		notify-send "$(basename $dir) is being merged now"
+    git merge
+  fi
 done
