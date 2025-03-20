0ae871945fc61392506aefc8f19ba7de281c2a60
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    simplified notifs

--- gitstatus.sh
+++ gitstatus.sh
@@ -9,22 +9,22 @@ for dir in $HOME/repos/*/; do
   git_results="$(git -C $dir status &)"
   if [[ $(echo "$git_results" | grep "Your branch is behind") ]]; then
     echo "Sending notification: branch is behind"
-    notify-send "$(basename $dir) is behind" "Branch is behind"
+    notify-send "$(basename $dir) is behind"
   fi
   if [[ $(echo "$git_results" | grep "Your branch is ahead") ]]; then
     echo "Sending notification: branch is ahead"
-    notify-send "$(basename $dir) is ahead" "Branch is ahead"
+    notify-send "$(basename $dir) is ahead"
   fi
   if [[ $(echo "$git_results" | grep "have diverged") ]]; then
     echo "Sending notification: branch has diverged"
-    notify-send "$(basename $dir) has diverged" "Branch has diverged"
+    notify-send "$(basename $dir) has diverged"
   fi
   if [[ $(echo "$git_results" | grep "Changes to be comm") ]]; then
     echo "Sending notification: pending commits"
-    notify-send "$(basename $dir) has uncommitted changes" "Branch has pending commits"
+    notify-send "$(basename $dir) has uncommitted changes"
   fi
   if [[ $(echo "$git_results" | grep "Changes not staged") ]]; then
     echo "Sending notification: unstaged changes"
-    notify-send "$(basename $dir) has unstaged changes" "Branch has unstaged changes"
+    notify-send "$(basename $dir) has unstaged changes"
   fi
 done
