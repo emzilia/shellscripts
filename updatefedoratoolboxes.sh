90703a0391901adf76e468e26a7ce9b03e061107
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    changed image name to variable for easier updating

--- updatefedoratoolboxes.sh
+++ updatefedoratoolboxes.sh
@@ -1,9 +1,11 @@
 #!bin/bash
-#Loops through all podman containers, starting and running dnf update within ones using the fedora image, stopping them afterwards.
+#Loops through all podman containers, starting and running dnf update within the ones using the fedora toolbox image, stopping them afterwards.
+
+image='registry.fedoraproject.org/fedora-toolbox:37'
 
 for id in $(podman ps -aq); do
     podman start $id
-    if [[ $(podman inspect -f '{{.Config.Image}}' $id) == "registry.fedoraproject.org/fedora-toolbox:37" ]]; then
+    if [[ $(podman inspect -f '{{.Config.Image}}' $id) == "$image" ]]; then
         podman exec $id sudo dnf update -y
 	podman stop $id
     fi
