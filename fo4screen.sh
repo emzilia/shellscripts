a5dd07b5e6afa7ca93cb6982e18780cbf7090d18
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    streamlined script by making xrandrcheck a func

--- fo4screen.sh
+++ fo4screen.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
-process="Fallout4.exe"
-
-xrandrcheck=$(xrandr --listactivemonitors | awk '/Monitors:/{print $2}')
+xrandrcheck () {
+	local result=$(xrandr --listactivemonitors | awk '/Monitors:/{print $2}')
+	echo "$result"
+}
 
 xrandroff="xrandr --output DVI-D-0 --off"
 
 xrandreset="xrandr --output DVI-D-0 --auto --left-of DP-0"
 
-while pgrep "$process" >/dev/null; do
-	if [ "$xrandrcheck" -eq 2 ]; then
+while pgrep Fallout4.exe >/dev/null; do
+	if [ "$(xrandrcheck)" -eq 2 ]; then
 		eval "$xrandroff"
 	fi
-	sleep 1
+	sleep 5
 done
 
-eval "$xrandreset"
+if [ "$(xrandrcheck)" -eq 1 ]; then
+	eval "$xrandreset"
+fi
+
