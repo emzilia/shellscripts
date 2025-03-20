428552a03951f498843d539369ca8890544fb147
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    revamped to use xpointerbarrier, which trivializes the process

---
+++ lock_mouse_to_monitor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# When run, creates a barrier the mouse can't traverse, for full-screen
+# apps that don't capture the mouse properly
+command="xpointerbarrier"
+
+# Game process to watch for
+process=Fallout4.exe
+
+# While game's process is running, if xpointerbarrier isn't running, run it
+while pgrep $process >/dev/null; do
+	if ! pgrep "$command" >/dev/null; then
+		$command 0 0 0 0 &
+	fi
+	sleep 5
+done
+
+# If game's process isn't running and xpointerbarrier still is, kill it
+if pgrep "$command" >/dev/null; then
+	pkill "$command"
+fi
+
