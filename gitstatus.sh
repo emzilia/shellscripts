41376b6d794b2c1c7ce53b237d7f89a9f9193d32
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    changed indent to 2spc

--- gitstatus.sh
+++ gitstatus.sh
@@ -4,29 +4,29 @@
 # notification is sent based on the git repo's status.
 
 for dir in $HOME/repos/*/; do
-	cd "$dir"
-	echo "Entering $dir"
-	git fetch
-	echo "Changes have been fetched"
-	git_results="$(git status)"
-	if [[ $(echo "$git_results" | grep "Your branch is behind") ]]; then
-		echo "Sending notification: branch is behind"
-		notify-send "$(basename $dir) is behind"
-	fi
-	if [[ $(echo "$git_results" | grep "Your branch is ahead") ]]; then
-		echo "Sending notification: branch is ahead"
-		notify-send "$(basename $dir) is ahead"
-	fi
-	if [[ $(echo "$git_results" | grep "have diverged") ]]; then
-		echo "Sending notification: branch has diverged"
-		notify-send "$(basename $dir) has diverged"
-	fi
-	if [[ $(echo "$git_results" | grep "Changes to be comm") ]]; then
-		echo "Sending notification: pending commits"
-		notify-send "$(basename $dir) has pending commits"
-	fi
-	if [[ $(echo "$git_results" | grep "Changes not staged") ]]; then
-		echo "Sending notification: unstaged changes"
-		notify-send "$(basename $dir) has unstaged changes"
-	fi
+  cd "$dir"
+  echo "Entering $dir"
+  git fetch
+  echo "Changes have been fetched"
+  git_results="$(git status)"
+  if [[ $(echo "$git_results" | grep "Your branch is behind") ]]; then
+    echo "Sending notification: branch is behind"
+    notify-send "$(basename $dir) is behind"
+  fi
+  if [[ $(echo "$git_results" | grep "Your branch is ahead") ]]; then
+    echo "Sending notification: branch is ahead"
+    notify-send "$(basename $dir) is ahead"
+  fi
+  if [[ $(echo "$git_results" | grep "have diverged") ]]; then
+    echo "Sending notification: branch has diverged"
+    notify-send "$(basename $dir) has diverged"
+  fi
+  if [[ $(echo "$git_results" | grep "Changes to be comm") ]]; then
+    echo "Sending notification: pending commits"
+    notify-send "$(basename $dir) has pending commits"
+  fi
+  if [[ $(echo "$git_results" | grep "Changes not staged") ]]; then
+    echo "Sending notification: unstaged changes"
+    notify-send "$(basename $dir) has unstaged changes"
+  fi
 done
