e19dffe988482f2b68133b14e914d40bbfc2e902
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    reformatted, made sexier

--- updatefedoratoolboxes.sh
+++ updatefedoratoolboxes.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#Loops through all podman containers, starting and running dnf update within the ones using the fedora toolbox image, stopping them afterwards.
+# Loops through all podman containers, starting and running dnf update within
+# the ones using the fedora toolbox image, stopping them afterwards.
 
 image='registry.fedoraproject.org/fedora-toolbox:37'
 
 for id in $(podman ps -aq); do
-    if [[ $(podman inspect -f '{{.Config.Image}}' $id) == "$image" ]]; then
-      podman start $id
-      podman exec $id sudo dnf update -y
-      podman stop $id
-    fi
+  if [[ $(podman inspect -f '{{.Config.Image}}' $id) == "$image" ]]; then
+    podman start $id
+    podman exec $id sudo dnf update -y
+    podman stop $id
+  fi
 done
