8fd8c458a558ad685c731c6642c6b5b5b6507cda
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    shortened notification

--- gitstatus.sh
+++ gitstatus.sh
@@ -5,18 +5,18 @@ for dir in $HOME/repos/*/; do
 	git fetch
 	git_results="$(git status)"
 	if [[ $(echo "$git_results" | grep "Your branch is behind") ]]; then
-		notify-send "Your branch is behind: $(basename $dir)" "Changes have been fetched and are ready to merge."
+		notify-send "Branch is behind: $(basename $dir)" "Changes have been fetched and are ready to merge."
 	fi
 	if [[ $(echo "$git_results" | grep "Your branch is ahead") ]]; then
-		notify-send "Your branch is ahead: $(basename $dir)" "Changes have been commited and are ready to push."
+		notify-send "Branch is ahead: $(basename $dir)" "Changes have been commited and are ready to push."
 	fi
 	if [[ $(echo "$git_results" | grep "have diverged") ]]; then
-		notify-send "Your branch has diverged: $(basename $dir)" "Intervention required to reconcile changes."
+		notify-send "Branch has diverged: $(basename $dir)" "Intervention required to reconcile changes."
 	fi
 	if [[ $(echo "$git_results" | grep "Changes to be committed:") ]]; then
-		notify-send "Your branch has pending commits: $(basename $dir)" "Changes have been staged and are ready to commit."
+		notify-send "Branch has pending commits: $(basename $dir)" "Changes have been staged and are ready to commit."
 	fi
 	if [[ $(echo "$git_results" | grep "Changes not staged for commit:") ]]; then
-		notify-send "Your branch has unstaged changes: $(basename $dir)" "Changes have been made and are ready to stage."
+		notify-send "Branch has unstaged changes: $(basename $dir)" "Changes have been made and are ready to stage."
 	fi
 done
