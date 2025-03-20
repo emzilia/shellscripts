3e29a0ded284e822d926523b68d1708a6e643cb0
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    removed junk

--- gitstatus/gitstatus-runner.sh
+++
@@ -1,5 +0,0 @@
-#! bin/bash
-bash $HOME/scripts/gitstatus/gitstatus-startpage.sh
-bash $HOME/scripts/gitstatus/gitstatus-startpagev2.sh
-bash $HOME/scripts/gitstatus/gitstatus-tagalogpy.sh
-bash $HOME/scripts/gitstatus/gitstatus-pwshpro.sh
