92d11082ef99190ea57fc2cab3ce5910b7885cc9
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    fixed indentation

--- change_conn_profile.sh
+++ change_conn_profile.sh
@@ -18,18 +18,18 @@ active="$(nmcli -f NAME c show --active | tail -n +2 | xargs)"
 
 # if the currently active profile gets selected, do nothing
 if [ "${choice}" = "${active}" ]; then
-	printf "You're already connected to %s\n" "${active}"
-	exit 0
+  printf "You're already connected to %s\n" "${active}"
+  exit 0
 fi
 
 # if the chosen profile exists within the profile list,
 case "${list}" in
-	*${choice}*)
-		printf "Connecting to: %s\n" "${choice}"
-		nmcli c up "${choice}"
-		;;
-	*)
-		printf "Profile %s not found" "${choice}"
-		;;
+  *${choice}*)
+    printf "Connecting to: %s\n" "${choice}"
+    nmcli c up "${choice}"
+    ;;
+  *)
+    printf "Profile %s not found" "${choice}"
+    ;;
 esac
 
