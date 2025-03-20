d26078363c3eecfc56abfdd0ca3a66107df517e7
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    nit-picky change, shortened variable by 1 character

--- gitstatus.sh
+++ gitstatus.sh
@@ -1,15 +1,15 @@
 #!bin/bash
 # Loops through all folders in the repos directory, running git fetch and git status and sending a notification if there are pending changes.
-for dirs in $HOME/repos/*/; do
-	cd "$dirs"
+for dir in $HOME/repos/*/; do
+	cd "$dir"
 	git fetch
 	if [[ $(git status | grep "Your branch is behind") ]]; then
-		notify-send "Your branch is behind: $(basename $dirs)" "Changes have been fetched and are ready to merge."
+		notify-send "Your branch is behind: $(basename $dir)" "Changes have been fetched and are ready to merge."
 	fi
 	if [[ $(git status | grep "Your branch is ahead") ]]; then
-		notify-send "Your branch is ahead: $(basename $dirs)" "Changes have been commited and are ready to push."
+		notify-send "Your branch is ahead: $(basename $dir)" "Changes have been commited and are ready to push."
 	fi
 	if [[ $(git status | grep "have diverged") ]]; then
-		notify-send "Your branch has diverged: $(basename $dirs)" "Intervention required to reconcile changes."
+		notify-send "Your branch has diverged: $(basename $dir)" "Intervention required to reconcile changes."
 	fi
 done
