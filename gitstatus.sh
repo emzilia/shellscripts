0032ecad846d62afe70fd98eba85202f518682e6
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    added notification for local branch being ahead

--- gitstatus.sh
+++ gitstatus.sh
@@ -6,6 +6,9 @@ for dirs in $HOME/repos/*/; do
 	if [[ $(git status | grep "Your branch is behind") ]]; then
 		notify-send "Your branch is behind: $(basename $dirs)" "Changes have been fetched and are ready to merge."
 	fi
+	if [[ $(git status | grep "Your branch is ahead") ]]; then
+		notify-send "Your branch is ahead: $(basename $dirs)" "Changes have been commited and are ready to push."
+	fi
 	if [[ $(git status | grep "have diverged") ]]; then
 		notify-send "Your branch has diverged: $(basename $dirs)" "Intervention required to reconcile changes."
 	fi
