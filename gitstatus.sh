e56f9b5000c117e25bb109e73b7f57690c4d858a
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    changed variable name for legibility

--- gitstatus.sh
+++ gitstatus.sh
@@ -1,9 +1,12 @@
 #!bin/bash
 # Loops through all folders in the repos directory, running git fetch and git status and sending a notification if there are pending changes.
-for d in $HOME/repos/*/; do
-	cd "$d"
+for dirs in $HOME/repos/*/; do
+	cd "$dirs"
 	git fetch
 	if [[ $(git status | grep "Your branch is behind") ]]; then
-		notify-send "A remote repository has been updated: $(basename $d)"
+		notify-send "Your branch is behind: $(basename $dirs)" "Changes have been fetched and are ready to merge."
+	fi
+	if [[ $(git status | grep "have diverged") ]]; then
+		notify-send "Your branch has diverged: $(basename $dirs)" "Intervention required to reconcile changes."
 	fi
 done
