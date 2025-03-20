5504bd4f5abc1d5d261a13e7b6dd82eaf91b80f3
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    bulk git push script starting point

---
+++ gitpush.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Loops through all folders in the $HOME/repos directory, running git fetch and saving the result of git status to a variable, which is then read and a notification is sent based on the git repo's status.
+for dir in $HOME/repos/*/; do
+	cd "$dir"
+	echo "Entering $dir"
+	git fetch
+	echo "Changes have been fetched"
+	git_results="$(git status)"
+	if [[ $(echo "$git_results" | grep "Your branch is behind") ]]; then
+		echo "Sending notification: branch is behind"
+		notify-send "Branch is behind: $(basename $dir)" "Changes have been fetched and are ready to merge."
+		git pull --rebase
+	fi
+done
