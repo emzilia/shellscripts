ed02b22f11748302fcbea1507c074fb173e0c59b
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    added comments and resetbar path

--- fo4screen.sh
+++ fo4screen.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
+# Runs xrandr --listactivemonitors and captures the number of actives monitors, 1 or 2
 xrandrcheck () {
 	local result=$(xrandr --listactivemonitors | awk '/Monitors:/{print $2}')
 	echo "$result"
 }
 
+# Disables secondary monitor
 xrandroff="xrandr --output DVI-D-0 --off"
 
+# Re-enables secondary monitor and places it back in its spot
 xrandreset="xrandr --output DVI-D-0 --auto --left-of DP-0"
 
+# Runs script to restart polybar
+resetbar="~/.config/polybar/launch.sh"
+
+# While Fallout 4 is running, if 2 monitors are active disable the secondary one
+# Checks every 5 seconds
 while pgrep Fallout4.exe >/dev/null; do
 	if [ "$(xrandrcheck)" -eq 2 ]; then
 		eval "$xrandroff"
@@ -16,7 +24,10 @@ while pgrep Fallout4.exe >/dev/null; do
 	sleep 5
 done
 
+# If Fallout 4 isn't running and only 1 monitor is active, re-enable the secondary
+# monitor and reset polybar
 if [ "$(xrandrcheck)" -eq 1 ]; then
 	eval "$xrandreset"
+	eval "$resetbar"
 fi
 
