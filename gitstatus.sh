7b961957f03d9823eece1aac016a1b7ddca123e6
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    saved git status to variable to remove redundant command calls

--- gitstatus.sh
+++ gitstatus.sh
@@ -1,15 +1,16 @@
-#!bin/bash
+#!/bin/bash
 # Loops through all folders in the repos directory, running git fetch and git status and sending a notification if there are pending changes.
 for dir in $HOME/repos/*/; do
 	cd "$dir"
 	git fetch
-	if [[ $(git status | grep "Your branch is behind") ]]; then
+	git_results="$(git status)"
+	if [[ $(echo "$git_results" | grep "Your branch is behind") ]]; then
 		notify-send "Your branch is behind: $(basename $dir)" "Changes have been fetched and are ready to merge."
 	fi
-	if [[ $(git status | grep "Your branch is ahead") ]]; then
+	if [[ $(echo "$git_results" | grep "Your branch is ahead") ]]; then
 		notify-send "Your branch is ahead: $(basename $dir)" "Changes have been commited and are ready to push."
 	fi
-	if [[ $(git status | grep "have diverged") ]]; then
+	if [[ $(echo "$git_results" | grep "have diverged") ]]; then
 		notify-send "Your branch has diverged: $(basename $dir)" "Intervention required to reconcile changes."
 	fi
 done
