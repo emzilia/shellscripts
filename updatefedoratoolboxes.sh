63224ee85cb04f29d88382427db3462c1d0d97a5
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    fixed spacing of commands within if block

--- updatefedoratoolboxes.sh
+++ updatefedoratoolboxes.sh
@@ -5,8 +5,8 @@ image='registry.fedoraproject.org/fedora-toolbox:37'
 
 for id in $(podman ps -aq); do
     if [[ $(podman inspect -f '{{.Config.Image}}' $id) == "$image" ]]; then
-    	podman start $id
-        podman exec $id sudo dnf update -y
-	podman stop $id
+      podman start $id
+      podman exec $id sudo dnf update -y
+      podman stop $id
     fi
 done
