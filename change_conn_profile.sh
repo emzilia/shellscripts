ed44492359733dd91772eaecf497fe7af0be7af2
emzilia <em@zilia.dev>
2025-03-20 17:43:36 -0400

    split list and dmenu into separate variables, more robust

--- change_conn_profile.sh
+++ change_conn_profile.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 # Switch between connection profiles with dmenu
 
-choice="$(nmcli -f NAME c show | tail -n +2 | dmenu)"
+list="$(nmcli -f NAME c show | tail -n +2)"
+
+choice="$(echo "$list" | dmenu)"
 
 active="$(nmcli -f NAME c show --active | tail -n +2)"
 
 if [ "$choice" = "$active" ]; then
 	echo "You're already connected to $active"
-else 
+elif [[ "$list" = *"$choice"* ]]; then
 	echo "Connecting to: $choice"
 	nmcli c up $choice
+else
+	echo "Profile $choice not found"
 fi
