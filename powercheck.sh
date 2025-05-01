4d6beb98dc9750e9dbdd7270b09f18adb1938c4c
emzilia <em@zilia.dev>
2025-05-01 12:39:35 -0400

    added comments, trimmed whitespace

--- powercheck.sh
+++ powercheck.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
-
+# When AC power gets connected, kill swayidle.
+# When AC power gets disconnected, start swayidle
 printf "Power Check: Initialized\n"
-
-while [ 1 ]; do
+while true; do
   status="$(cat /sys/class/power_supply/AC/online)"
   case "$status" in
     "1")
       if pkill -e swayidle; then
         printf "Power Check: AC connection established, ending screen idle\n" 
-      fi
-      ;;
+      fi ;;
     "0")
-     if ! pgrep -l swayidle >/dev/null; then
+     if ! pgrep swayidle >/dev/null; then
        swayidle -w &
        printf "Power Check: AC connection terminated, restarting screen idle\n"
-     fi
-     ;;
+     fi ;;
    esac
    sleep 60
 done
