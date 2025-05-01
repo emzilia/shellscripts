19ceea0a7046ffb57e49c8e6d38bf4c4ffc468b0
emzilia <em@zilia.dev>
2025-05-01 11:06:04 -0400

    new script for managing swayidle with ac power status

---
+++ powercheck.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+printf "Power Check: Initialized\n"
+
+while [ 1 ]; do
+  status="$(cat /sys/class/power_supply/AC/online)"
+  case "$status" in
+    "1")
+      if pkill -e swayidle; then
+        printf "Power Check: AC connection established, ending screen idle\n" 
+      fi
+      ;;
+    "0")
+     if ! pgrep -l swayidle; then
+       swayidle -w &
+       printf "Power Check: AC connection terminated, restarting screen idle\n"
+     fi
+     ;;
+   esac
+   sleep 60
+done
