b746ee67b1efa9291d0973d20e311c61cab6e523
emzilia <em@zilia.dev>
2025-11-04 00:18:16 -0500

    refactored into single execution to run on a systemd timer instead

--- powercheck.sh
+++ powercheck.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 # When AC power gets connected, kill swayidle.
 # When AC power gets disconnected, start swayidle
-printf "Power Check: Initialized\n"
-while true; do
-  status="$(cat /sys/class/power_supply/AC/online)"
-  case "$status" in
-    "1")
-      if pkill -e swayidle; then
-        printf "Power Check: AC connection established, ending screen idle\n" 
-      fi ;;
-    "0")
-     if ! pgrep -x swayidle >/dev/null; then
-       swayidle -w &
-       printf "Power Check: AC connection terminated, restarting screen idle\n"
-     fi ;;
-   esac
-   sleep 60
-done
+status="$(cat /sys/class/power_supply/AC/online)"
+printf "Power Check Status: %s\n" "$status"
+case "$status" in
+  "1")
+    if pkill -e swayidle; then
+      printf "Power Check: AC connection established, ending screen idle\n" 
+    fi ;;
+  "0")
+   if ! pgrep -x swayidle; then
+     swaymsg exec 'swayidle -w &'
+     printf "Power Check: AC connection terminated, restarting screen idle\n"
+   fi ;;
+esac
